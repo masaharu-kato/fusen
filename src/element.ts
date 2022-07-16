e6a6419589c23735827859fdff57fa5f0bb49e16
Masaharu Kato <fudai.mk@gmail.com>
2022-07-16 09:28:13 +0900

    improve element processes (WIP)

--- src/element.ts
+++ src/element.ts
@@ -1,62 +1,214 @@
 import { Evaluator } from './evaluator';
 import { TemplateText } from './tmpl-text';
 
-abstract class FusNode {
+export type NodeType = FusNode | Node;
+
+export abstract class FusNode {
   abstract make_nodes(evl: Evaluator): Generator<Node>;
 }
 
-abstract class SingleNode extends FusNode {
+export abstract class FusFlowNode extends FusNode {}
+
+abstract class SingleFusNode extends FusNode {
+  base_node: Node;
+
+  constructor(base_node: Node) {
+    super();
+    this.base_node = base_node;
+  }
+
   abstract make_node(evl: Evaluator): Node;
+
+  abstract make_node_with_base(node: Node, evl: Evaluator): void;
 
   *make_nodes(evl: Evaluator): Generator<Node> {
     yield this.make_node(evl);
   }
 }
 
-class MultipleNode extends FusNode {
-  nodes: Array<FusNode>;
+export function* make_nodes(node: NodeType | null, evl: Evaluator): Generator<Node> {
+  if (node !== null) {
+    if (node instanceof FusNode) {
+      yield* node.make_nodes(evl);
+    } else {
+      yield node;
+    }
+  }
+}
 
-  constructor(nodes: Array<FusNode>) {
+class FusNodeList extends FusNode {
+  nodes: Array<NodeType>;
+
+  constructor(nodes: Array<NodeType>) {
     super();
     this.nodes = nodes;
   }
 
+  // *get_base_nodes(): Generator<Node> {
+  //   for (const node of this.nodes) {
+  //     if (node instanceof FusNode) {
+  //       if (node instanceof SingleFusNode) yield node.base_node;
+  //     } else {
+  //       yield node;
+  //     }
+  //   }
+  // }
+
+  make_nodes_with_base(elm: Element, evl: Evaluator) {
+    const elm_cnodes = [...elm.childNodes];
+
+    //  No existing base nodes
+    if (!elm_cnodes.length) {
+      for (const node of this.nodes) {
+        if (node instanceof FusNode) elm.append(...node.make_nodes(evl));
+      }
+    }
+    //  Base nodes exists
+    else {
+      let next_cnode_i = 0; //  Counter for elm_cnodes
+      for (const node of this.nodes) {
+        if (node instanceof FusNode) {
+          if (node instanceof SingleFusNode) {
+            //  Update existing base node
+            node.make_node_with_base(elm_cnodes[next_cnode_i], evl);
+            next_cnode_i++;
+          } else {
+            //  Process and add new nodes before the next base node
+            elm_cnodes[next_cnode_i].before(...node.make_nodes(evl));
+          }
+        } else {
+          next_cnode_i++;
+        }
+        if (next_cnode_i >= elm_cnodes.length) throw new Error('Base element child nodes out of range.');
+      }
+    }
+  }
+
   *make_nodes(evl: Evaluator): Generator<Node> {
-    for (const node of this.nodes) yield* node.make_nodes(evl);
+    for (const node of this.nodes) yield* make_nodes(node, evl);
   }
 }
 
-class PlainTextNode extends SingleNode {
-  text_node: Node;
-  constructor(text: string) {
-    super();
-    this.text_node = document.createTextNode(text);
-  }
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  make_node(evl: Evaluator): Node {
-    return this.text_node;
-  }
-}
+export type ChildNodeType = FusNodeList | Node | null;
 
-class TemplateTextNode extends SingleNode {
+class TemplateTextNode extends SingleFusNode {
   tmpl_text: TemplateText;
   constructor(tmpl_text: TemplateText) {
-    super();
+    super(document.createTextNode(''));
     this.tmpl_text = tmpl_text;
   }
+
+  make_node_with_base(elm: Node, evl: Evaluator): void {
+    elm.textContent = this.tmpl_text.make_text(evl);
+  }
+
   make_node(evl: Evaluator): Node {
     return document.createTextNode(this.tmpl_text.make_text(evl));
   }
 }
 
-abstract class FlowNode extends FusNode {}
+export class FusElement extends SingleFusNode {
+  tmpl_attrs: { [name: string]: TemplateText };
+  child: ChildNodeType;
 
-class FusIf extends FlowNode {
+  constructor(elm: Element) {
+    super(elm);
+
+    //  Process attributes
+    this.tmpl_attrs = {};
+    for (const attr of [...elm.attributes]) {
+      const tmpl = new TemplateText(attr.value);
+      //  If the attribute value contains codes, add to template list and remove original attribute
+      if (tmpl.has_codes()) {
+        this.tmpl_attrs[attr.name] = tmpl;
+        elm.removeAttribute(attr.name);
+      }
+    }
+
+    //  Process child nodes
+    this.child = process_nodes([...elm.childNodes]);
+  }
+
+  make_node_with_base(node: Node, evl: Evaluator): void {
+    if (!(node instanceof Element)) throw new TypeError('Invalid type of base node.');
+    const elm = node;
+    for (const [name, tmpl] of Object.entries(this.tmpl_attrs)) {
+      elm.setAttribute(name, tmpl.make_text(evl));
+    }
+    if (this.child instanceof FusNodeList) this.child.make_nodes_with_base(elm, evl);
+  }
+
+  make_node(evl: Evaluator): Node {
+    const node = this.base_node.cloneNode(true);
+    this.make_node_with_base(node, evl);
+    return node;
+  }
+}
+
+export function process_nodes(nodes: Array<ChildNode>): FusNodeList {
+  const result: Array<NodeType> = [];
+
+  //  Process nodes
+  for (const node of nodes) {
+    let last_fus_node = null;
+
+    //  Element
+    if (node instanceof Element) {
+      const elm = node;
+      const tag = elm.tagName.toLowerCase();
+      const attrs = Object.fromEntries([...elm.attributes].map((attr) => [attr.name, attr.value]));
+      //  Fus element
+      if (tag.startsWith('fus-')) {
+        const child = process_nodes([...node.childNodes]);
+        const new_fus_node = process_fus_tag_element(tag, attrs, child, last_fus_node);
+        if (new_fus_node !== null) result.push(new_fus_node);
+        last_fus_node = new_fus_node;
+        elm.remove();
+      }
+      //  Normal element which may have Fus attributes
+      else {
+        // let fus_attr_name = null;
+        // for (const name in attrs) {
+        //   if (name.startsWith('fus-')) {
+        //     fus_attr_name = name;
+        //     break;
+        //   }
+        // }
+        // if (fus_attr_name) {
+        //   //  Fus attribute exists
+        //   const fus_attr_value = attrs[fus_attr_name];
+        //   elm.removeAttribute(fus_attr_name);
+        //   const child = process_nodes([elm]);
+        //   const new_fus_node = process_fus_tag_element(elm, tag, attrs, child, last_fus_node);
+        //   if (new_fus_node !== null) result.push(new_fus_node);
+        //   elm.remove();
+        // } else {
+        //   result.push(new FusElement(elm));
+        // }
+        result.push(new FusElement(elm));
+      }
+
+      //  Text Node
+    } else {
+      const text = node.textContent;
+      if (text !== null) {
+        const tmpl = new TemplateText(text);
+        if (tmpl.has_codes()) {
+          result.push(new TemplateTextNode(tmpl));
+          node.remove();
+        }
+      }
+    }
+  }
+  return new FusNodeList(result);
+}
+
+class FusIf extends FusFlowNode {
   expr: string;
-  node_true: FusNode;
-  node_false: FusNode | null;
+  node_true: ChildNodeType;
+  node_false: ChildNodeType;
 
-  constructor(expr: string, node_true: FusNode, node_false: FusNode | null = null) {
+  constructor(expr: string, node_true: ChildNodeType, node_false: ChildNodeType = null) {
     super();
     this.expr = expr;
     this.node_true = node_true;
@@ -65,20 +217,20 @@ class FusIf extends FlowNode {
 
   *make_nodes(evl: Evaluator): Generator<Node> {
     if (evl.eval_bool(this.expr)) {
-      yield* this.node_true.make_nodes(evl);
+      yield* make_nodes(this.node_true, evl);
     } else {
-      if (this.node_false !== null) yield* this.node_false.make_nodes(evl);
+      yield* make_nodes(this.node_false, evl);
     }
   }
 }
 
-class FusFor extends FlowNode {
+class FusFor extends FusFlowNode {
   iter_name: string;
   val_name: string;
-  node_iter: FusNode;
-  node_else: FusNode | null;
+  node_iter: ChildNodeType;
+  node_else: ChildNodeType;
 
-  constructor(iter_name: string, val_name: string, node_iter: FusNode, node_else: FusNode | null = null) {
+  constructor(iter_name: string, val_name: string, node_iter: ChildNodeType, node_else: ChildNodeType = null) {
     super();
     this.iter_name = iter_name;
     this.val_name = val_name;
@@ -90,103 +242,28 @@ class FusFor extends FlowNode {
     let cnt = 0;
     for (const val of evl.eval_iter(this.iter_name)) {
       const c_evl = evl.copy_with_locals({ [this.val_name]: val });
-      yield* this.node_iter.make_nodes(c_evl);
+      yield* make_nodes(this.node_iter, c_evl);
       cnt++;
     }
-    if (!cnt) {
-      if (this.node_else !== null) yield* this.node_else.make_nodes(evl);
-    }
+    if (!cnt) yield* make_nodes(this.node_else, evl);
   }
 }
 
-export class FusElement extends SingleNode {
-  base_elm: Element;
-  tmpl_attrs: { [name: string]: TemplateText };
-  child: FusNode | null;
-
-  constructor(tag: string, attrs: { [name: string]: string }, child: FusNode | null = null) {
-    super();
-
-    //  Create base element
-    this.base_elm = document.createElement(tag);
-
-    //  Process attributes
-    this.tmpl_attrs = {};
-    for (const [name, value] of Object.entries(attrs)) {
-      const tmpl = new TemplateText(value);
-      if (tmpl.has_codes()) {
-        this.tmpl_attrs[name] = tmpl;
-      } else {
-        this.base_elm.setAttribute(name, value);
-      }
-    }
-
-    //  Process child nodes
-    this.child = child;
-  }
-
-  make_node(evl: Evaluator): Node {
-    const elm = <Element>this.base_elm.cloneNode();
-    for (const [name, tmpl] of Object.entries(this.tmpl_attrs)) {
-      elm.setAttribute(name, tmpl.make_text(evl));
-    }
-    if (this.child !== null) {
-      for (const c_elm of this.child.make_nodes(evl)) elm.append(c_elm);
-    }
-    return elm;
-  }
-}
-
-export function* make_fus_nodes(nodes: Array<Node>): Generator<FusNode> {
-  let last_fus_elm: FusNode | null = null;
-  //  Process nodes
-  for (const node of nodes) {
-    let c_fus_elm = null;
-    const child = new MultipleNode([...make_fus_nodes([...node.childNodes])]);
-    if (node instanceof Element) {
-      const elm = node;
-      const tag = elm.tagName.toLowerCase();
-      //
-      if (tag == 'fus-if') {
-        const expr = elm.getAttribute('v');
-        if (!expr) throw new Error('Expression not specified.');
-        c_fus_elm = new FusIf(expr, child);
-      } //
-      else if (tag == 'fus-else') {
-        if (!(last_fus_elm instanceof FusIf)) throw new Error('Invalid use of Fus-else.');
-        last_fus_elm.node_false = child;
-      } //
-      else if (tag == 'fus-for') {
-        const iter_name = elm.getAttribute('v');
-        if (!iter_name) throw new Error('Variable not specified.');
-        const val_name = elm.getAttribute('as') || '';
-        c_fus_elm = new FusFor(iter_name, val_name, child);
-      } //
-      else if (tag == 'fus-for-else') {
-        if (!(last_fus_elm instanceof FusFor)) throw new Error('Invalid use of Fus-for-else.');
-        last_fus_elm.node_else = child;
-      } else {
-        const attrs = Object.fromEntries([...elm.attributes].map((v) => [v.name, v.value]));
-        // if (attrs['fus-if']) return new FusIf(attrs['fus-if'], child)
-        // if (attrs['fus-else']) return new FusIf(attrs['fus-if'], child)
-
-        c_fus_elm = new FusElement(tag, attrs, child);
-      }
-      //
-    } //
-    else {
-      const text = node.textContent;
-      if (text !== null) {
-        const tmpl = new TemplateText(text);
-        if (tmpl.has_codes()) {
-          c_fus_elm = new TemplateTextNode(tmpl);
-        } else {
-          c_fus_elm = new PlainTextNode(text);
-        }
-      }
-    }
-    //  Output current fus-element
-    if (c_fus_elm !== null) yield c_fus_elm;
-    last_fus_elm = c_fus_elm;
+export function process_fus_tag_element(tag: string, attrs: { [name: string]: string }, child: ChildNodeType, last_fus_node: FusNode | null): FusNode | null {
+  switch (tag) {
+    case 'fus-if':
+      return new FusIf(attrs.v, child);
+    case 'fus-else':
+      if (!(last_fus_node instanceof FusIf)) throw new Error('Invalid use of Fus-else.');
+      last_fus_node.node_false = child;
+      return null;
+    case 'fus-for':
+      return new FusFor(attrs.v, attrs.as, child);
+    case 'fus-for-else':
+      if (!(last_fus_node instanceof FusFor)) throw new Error('Invalid use of Fus-for-else.');
+      last_fus_node.node_else = child;
+      return null;
+    default:
+      throw new Error('Invalid fus element.');
   }
 }
--- tests/element.test.ts
+++ tests/element.test.ts
@@ -1,4 +1,4 @@
-import { make_fus_nodes, FusElement } from '../src/element';
+import { FusElement, process_nodes } from '../src/element';
 
 test.each([
   [1, '<div></div>', 'div', {}, {}],
@@ -8,13 +8,61 @@ test.each([
   [5, '<div id="{{data.id}}" class="hoge"></div>', 'div', {class: 'hoge'}, {id: {pos: 0, code: 'data.id'}}],
   [6, '<div id="{{data.id}}" class="{{myclass}}"></div>', 'div', {}, {id: {pos: 0, code: 'data.id'}, class: {pos: 0, code: 'myclass'}}],
   [7, '<div id="{{data.id}}" class="foo {{myclass}} bluh" data-bar="bazbaz"></div>', 'div', {'data-bar': 'bazbaz'}, {id: {pos: 0, code: 'data.id'}, class: {pos: 4, code: 'myclass'}}],
-])('test case %d', (no, html, tag, attrs, tmpl_attrs) => {
+])('Single element: case %d', (no, html, tag, attrs, tmpl_attrs) => {
   document.body.innerHTML = html;
-  const fus_nodes = [...make_fus_nodes([...document.body.childNodes])]
-  const fus_node = fus_nodes[0];
+  const fus_node = new FusElement(<Element>document.body.childNodes[0]);
   if(!(fus_node instanceof FusElement)) throw new TypeError();
-  expect(fus_node.base_elm.tagName.toLowerCase()).toEqual(tag);
-  expect([...fus_node.base_elm.attributes].map((v) => [v.name, v.value])).toEqual(Object.entries(attrs));
+  const base_elm = <Element>fus_node.base_node;
+  expect(base_elm.tagName.toLowerCase()).toBe(tag);
+  expect([...base_elm.attributes].map((v) => [v.name, v.value])).toEqual(Object.entries(attrs));
   expect(Object.entries(fus_node.tmpl_attrs).map(([k, v]) => [k, v.pos_codes[0]])).toEqual(Object.entries(tmpl_attrs));
 });
 
+test.each([
+  [0, '', [{tag:'div', attrs:{}, tmpl_attrs:{}}]],
+  [1, '<div class="hoge"></div>', [{tag:'div', attrs:{class: 'hoge'}, tmpl_attrs:{}}]],
+  [2, '<div id="myelm123" class="hoge"></div><div id="bc546" class="booboo" title="click here"></div>',
+    [ {tag:'div', attrs:{id: 'myelm123', class: 'hoge'}, tmpl_attrs:{}},
+      {tag:'div', attrs:{id: 'bc546', class: 'booboo', title: 'click here'}, tmpl_attrs:{}} ]],
+  [3, '<div id="myelm123" class="name-{{name1}}"></div><div id="bc546" class="name-{{hoge.name2}}" title="hey {{hoge.name2}}!"></div>',
+    [ {tag:'div', attrs:{id: 'myelm123'}, tmpl_attrs:{class: {pos: 5, code:'name1'}}}, 
+      {tag:'div', attrs:{id: 'bc546'}, tmpl_attrs:{class: {pos: 5, code: 'hoge.name2'}, title: {pos: 4, code: 'hoge.name2'}}}]],
+  [4, '<div class="hoge" value="{{.hoge}}"></div><a class="foo" value="{{info.foo}}"></a><span class="class-{{barbaz}}" value="{{data.bar.baz}}"></span>',
+    [ {tag:'div', attrs:{class: 'hoge'}, tmpl_attrs:{value: {pos: 0, code:'.hoge'}}}, 
+      {tag:'a', attrs:{class: 'foo'}, tmpl_attrs:{value: {pos: 0, code:'info.foo'}}}, 
+      {tag:'span', attrs:{}, tmpl_attrs:{class: {pos: 6, code:'barbaz'}, value: {pos: 0, code:'data.bar.baz'}}} ]],
+  [5, '<div class="hoge" value="{{.hoge}}"></div><div id="sep9876" class="separator"></div><a class="foo" value="{{info.foo}}"></a><div class="separator"></div><span class="quote-beg"></span><span class="class-{{barbaz}}" value="{{data.bar.baz}}"></span><span class="quote-end"></span>',
+    [ {tag:'div', attrs:{class: 'hoge'}, tmpl_attrs:{value: {pos: 0, code:'.hoge'}}},
+      {tag:'div', attrs:{id: 'sep9876', class:'separator'}, tmpl_attrs: {}},
+      {tag:'a', attrs:{class: 'foo'}, tmpl_attrs:{value: {pos: 0, code:'info.foo'}}}, 
+      {tag:'div', attrs:{class:'separator'}, tmpl_attrs: {}},
+      {tag:'span', attrs:{class:'quote-beg'}, tmpl_attrs: {}},
+      {tag:'span', attrs:{}, tmpl_attrs:{class: {pos: 6, code:'barbaz'}, value: {pos: 0, code:'data.bar.baz'}}},
+      {tag:'span', attrs:{class:'quote-end'}, tmpl_attrs: {}},
+    ]],
+])('Multiple element: case %d', (no: number, html: string, infos: any[]) => {
+  document.body.innerHTML = html;
+  const fus_nodes = process_nodes([...document.body.childNodes]);
+  for(let i=0; i<fus_nodes.nodes.length; i++) {
+    const fus_node = <FusElement>fus_nodes.nodes[i]
+    const info = infos[i]
+    const base_elm = <Element>fus_node.base_node;
+    expect(base_elm.tagName.toLowerCase()).toBe(info.tag);
+    expect([...base_elm.attributes].map((v) => [v.name, v.value])).toEqual(Object.entries(info.attrs));
+    expect(Object.entries(fus_node.tmpl_attrs).map(([k, v]) => [k, v.pos_codes[0]])).toEqual(Object.entries(info.tmpl_attrs));
+  }
+});
+
+
+const data = {name: 'hoge', baz:'foo1234', 'view-count': 123, p:{x: 30.5, y: -67.62, z: 9.2452}, color:{rgb:{r:18, g:152, b:8}, hsv:{h:210, s:79, v:34}}};
+
+// test.each([
+//   [1, '<div></div>', '<div></div>'],
+//   [2, '<div>{{name}}</div>', '<div>hoge</div>'],
+// ])('test case %d', (no, tmpl_html, result) => {
+//   document.body.innerHTML = tmpl_html;
+//   const fus_nodes = [...make_fus_nodes([...document.body.childNodes])]
+//   const fus_node = fus_nodes[0];
+//   if(!(fus_node instanceof FusElement)) throw new TypeError();
+//   expect(fus_node.base_elm.tagName.toLowerCase()).toEqual(tag);
+// });
